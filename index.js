561425a522e6f37114e878af0dadb2a7618b8b25
Manhhao <62149544+Manhhao@users.noreply.github.com>
2021-09-15 04:50:51 +0200

    Revert "damn"
    This reverts commit 88cebbb5e147f586c6457e391b8f975d07a54701.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const { token, prefix } = require("./config.json");
+const ytdl = require("ytdl-core");
 const ytsr = require ("ytsr");
 
 const client = new Discord.Client();
