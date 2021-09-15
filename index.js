c0b79b4fd59c86a8259271573a0e38e56626ba17
Manhhao <62149544+Manhhao@users.noreply.github.com>
2021-09-15 16:41:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function embedded_bold(msg){
     return message;
 }; 
 
-async function do_8ball() {
+function do_8ball() {
     var rand = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes – definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.',
     'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.',
     'Don’t count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
